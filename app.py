07f4395090a9c3923d053999d93e4596ad90d06c
r3tiu <tbp@sv-tel.ru>
2023-05-12 16:00:20 +0300

    + доработки

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ import pandas as pd
 import numpy as np
 import openpyxl
 import xlsxwriter
+from transliterate import translit, get_available_language_codes
 from io import BytesIO
 from config import DRIVER, SERVER, PORT, USER, PASSW, LANGUAGE, CLIENT_HOST_NAME, CLIENT_HOST_PROC, APPLICATION_NAME, SECRET_KEY
 
@@ -68,24 +69,37 @@ class Reports(object):
 				reports_list.append(dict(zip(columns_name,row)))
 			cursor.close()
 			return reports_list
-	def makeExcel():
-		df_1 = pd.DataFrame(np.random.randint(0,10,size=(10,4)), columns=list('ABCD'))
-		output = BytesIO()
-		writer = pd.ExcelWriter(output, engine='xlsxwriter')
-		df_1.to_excel(writer,startrow=0, merge_cells=False, sheet_name='Sheet_1')
-		workbook = writer.book
-		worksheet = writer.sheets["Sheet_1"]
-		format = workbook.add_format()
-		format.set_bg_color('#eeeeee')
-		worksheet.set_column(0,9,28)
-		writer.close()
-		output.seek(0)
-		return send_file(output,download_name="testing.xlsx",as_attachment=True)
+
+	def getNextReportId():
+		cursor = get_connection()
+		cursor.execute("select max(ID) as MAX_ID from SV..TBP_WEB_REPORTS_LIST")
+		#columns_name = [column[0] for column in cursor.description]
+		#id = []
+		#for row in cursor.fetchall():
+		#	id.append(dict(zip(columns_name,row)))
+		next_id = cursor.fetchall()[0][0] +1
+		cursor.close()
+		return next_id
+
+	def addReport(visible, isgroup, date_req, parent_id=None, name=None, exec_type=None, exec_path=None, method_name=None):
+		nextId = Reports.getNextReportId()
+		cursor = get_connection()
+		try:
+			print(f"insert into SV..TBP_WEB_REPORTS_LIST(ID, PARENT_ID, NAME, VISIBLE, DATE_ADD, EXEC_TYPE, EXEC_PATH, METHOD_NAME, IS_GROUP, DATE_REQ)"
+		                   f" VALUES ({nextId},{parent_id}, '{name}', {visible}, getdate(),'{exec_type}','{exec_path}','{method_name}',{isgroup},{date_req})")
+
+			#cursor.commit()
+		except Exception as e:
+			#cursor.rollback()
+			return e
+		cursor.close()
+		return True
 
 
 class User(object):
 
 	def getUser(username, password):
+
 		if username and password is not None:
 			cursor = get_connection()
 			cursor.execute(f"select USER_CODE,rtrim(USER_NAME) as USER_NAME,rtrim(USER_PASSWORD) as USER_PASSWORD,"
@@ -111,11 +125,15 @@ class User(object):
 				session['Login'] = Login
 			else:
 				#user_info = User.getUser(Login, Password)
-				session['logged_in'] = True
-				session['Login'] = Login
 				for item in user:#user_info:
+					session['logged_in'] = True
+					session['Login'] = item['USER_NAME']
+					session['UserStatus'] = item['USER_STATUS']
 					session['FullName'] = item['FULL_NAME']
+					session['GroupCode'] = item['GROUP_CODE']
+					session['UserActive'] = item['USER_ACTIVE']
 			return True
+			print(session)
 
 	def logout(self):
 		session.pop('logged_in', False)
@@ -137,12 +155,33 @@ def form_authorization():
 		else:
 			User.login(Login, Password)
 			for item in user:
-				if item['USER_NAME'] == Login and check_password_hash(psw_hash, item['USER_PASSWORD']) is True or g.user is not None:
+				if item['USER_NAME'] == Login and check_password_hash(psw_hash, item['USER_PASSWORD']) is True:
 					return redirect('/index')
 				else:
 					return render_template('auth_bad.html')
 	elif request.method == 'GET':
 		return render_template('auth.html')
+
+
+@app.route('/admin', methods = ['GET', 'POST'])
+def report_add():
+	if request.method == 'GET':
+		if session.get('logged_in') == True and translit(session.get('UserStatus'), language_code='ru', reversed=True) == 'A':
+			return render_template('reportadd.html', full_name = session.get('FullName'), status = session.get('UserStatus'))
+		else:
+			return render_template('noaccess.html', full_name = session.get('FullName'))
+	elif request.method == 'POST':
+		result = Reports.addReport(
+		                  request.form.get('ParentId'),
+		                  request.form.get('ReportName'),
+		                  request.form.get('Visible'),
+		                  request.form.get('ExecType'),
+		                  request.form.get('ExecPath'),
+		                  request.form.get('MethodName'),
+		                  request.form.get('DateReq')
+		                  )
+		print(result)
+
 
 
 @app.route('/index', methods=['GET','POST'])
